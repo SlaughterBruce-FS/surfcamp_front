182c151f5cc0dd8e1dd3b67736809608a398f1c0
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2024-06-01 00:37:16 -0400

    made article paragraphs responsive

--- app/_components/Blog/ParagraphWithImage.tsx
+++ app/_components/Blog/ParagraphWithImage.tsx
@@ -22,9 +22,11 @@ export interface paraWithImage {
 function ParagraphWithImage({ data }: paraWithImage) {
 
     // const imgUrl = data.image.data.attributes.url
+    const flexDirection = data.imageshowright ? 'flex-col' : 'flex-col-reverse';
+    const mdFlexDirection = data.imageshowright ? 'md:flex-row' : 'md:flex-row-reverse';
 
     return (
-        <div className={`flex ${data.imageshowright ? 'flex-row' : 'flex-row-reverse'}  gap-12 mb-20`}>
+        <div className={`flex flex-col ${data.imageshowright ? ' md:flex-row' : ' md:flex-row-reverse'} gap-12 mb-20`}>
             <div className='flex-1'>
                 <p className='text-4xl leading-10 '>{data.paragraph}</p>
             </div>
